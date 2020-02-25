d2a04be6e1eb46ff9e42fe393ce00f2391ba70bf
quinnlu2001 <45461949+quinnlu2001@users.noreply.github.com>
2020-02-25 15:17:31 -0600

    made canoe move with mouse

--- InteractiveScene/sketch.js
+++ InteractiveScene/sketch.js
@@ -1,4 +1,4 @@
-// Canoe Dodging
+// Canoe
 // Quinn Lu
 // XXX XX, 2020
 //
@@ -6,6 +6,9 @@
 // - describe what you did to take this project "above and beyond"
 
 let canoe;
+let x;
+let y;
+let dx = 1;
 
 function preload(){
   canoe = loadImage("assets/canoe.jpg");
@@ -13,9 +16,24 @@ function preload(){
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  x = width / 2;
+  y = height / 2;
 }
 
 function draw() {
+  console.log(dx, x, mouseX);
   background(220);
-  image(canoe, mouseX - 50, mouseY - 100, 100, 200);
+  // image(canoe, mouseX - 50, mouseY - 100, 100, 200);
+  image(canoe, x-50, y-100, 100, 200);
+  
+  x += dx;
+
+  if (mouseX > x){
+    dx = 5;
+  }
+
+  else if (mouseX < x){
+    dx = -5;
+  }
+
 }
