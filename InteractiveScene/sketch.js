eac2e0ce5e62fe301c1bde6881c413e9b17fde31
quinnlu2001 <45461949+quinnlu2001@users.noreply.github.com>
2020-02-26 14:43:54 -0600

    Some fixes

--- InteractiveScene/sketch.js
+++ InteractiveScene/sketch.js
@@ -6,16 +6,21 @@
 // - describe what you did to take this project "above and beyond"
 
 let canoe;
+let bg;
 let x;
 let y;
 let dx = 1;
 
 function preload(){
   canoe = loadImage("assets/canoe.jpg");
+  bg = loadImage("assets/blue.png");
 }
+
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background("blue");
   x = width / 2;
   y = height / 2;
 }
@@ -23,7 +28,7 @@ function setup() {
 function draw() {
   console.log(dx, x, mouseX);
   background(220);
-  // image(canoe, mouseX - 50, mouseY - 100, 100, 200);
+  image(bg,400,0,800,800);
   image(canoe, x-50, y-100, 100, 200);
   
   x += dx;
